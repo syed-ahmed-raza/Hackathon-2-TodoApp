c6fa08f9d9c4e679d883cd128103cf727530e771
syed-ahmed-raza <ar03110386073@gmail.com>
2025-12-28 18:03:53 +0500

    Fix: Simplified next config for static export

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig: any = {
+  output: 'export',
+  images: { unoptimized: true },
+  trailingSlash: true,  
 };
 
 export default nextConfig;
