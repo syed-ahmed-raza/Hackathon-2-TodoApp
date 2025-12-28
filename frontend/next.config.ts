142ce5b400092765e09ce4ae1aba6e0f6029e589
syed-ahmed-raza <ar03110386073@gmail.com>
2025-12-28 17:46:26 +0500

    Fix: Simplified next config for static export

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
-// Yahan hum ': any' laga rahe hain taake Red Lines khatam ho jayen
 const nextConfig: any = {
   output: 'export',
-  eslint: { ignoreDuringBuilds: true },
   images: { unoptimized: true },
 };
 
