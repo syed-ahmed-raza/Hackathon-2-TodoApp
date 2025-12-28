4ad350f63e49edae801a3423bba06621304b6da9
syed-ahmed-raza <ar03110386073@gmail.com>
2025-12-28 17:27:26 +0500

    Fix: Simplified next config for static export

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export',
+  eslint: { ignoreDuringBuilds: true },
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
